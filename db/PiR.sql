59a38deafec80dbae3381f5766012e1718fefc16
Ataor <slyguggi@gmail.com>
2020-12-01 15:47:47 +0100

    added correct types

--- db/PiR.sql
+++ db/PiR.sql
@@ -3,8 +3,9 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.1              
 -- * Generator date: Dec  4 2018              
--- * Generation date: Tue Dec  1 15:30:21 2020 
--- * Schema: MLD/1-1 
+-- * Generation date: Tue Dec  1 15:46:47 2020 
+-- * LUN file: F:\tools\uwamp\www\PiR\db\PiR.lun 
+-- * Schema: PiR/1-1 
 -- ********************************************* 
 
 
@@ -18,30 +19,30 @@ use PiR;
 -- Tables Section
 -- _____________ 
 
-create table t_client (
-     idClient char(1) not null,
-     cliFirstName char(1) not null,
-     cliLastName char(1) not null,
-     constraint ID_t_client_ID primary key (idClient));
-
 create table buy (
-     idClient char(1) not null,
-     idPrinter char(1) not null,
+     idClient int not null,
+     idPrinter int not null,
      constraint ID_buy_ID primary key (idClient, idPrinter));
 
+create table t_client (
+     idClient int not null auto_increment,
+     cliFirstName varchar(50) not null,
+     cliLastName varchar(50) not null,
+     constraint ID_t_client_ID primary key (idClient));
+
 create table t_printer (
-     idPrinter char(1) not null,
-     priBrand char(1) not null,
-     priModel char(1) not null,
+     idPrinter int not null auto_increment,
+     priBrand varchar(50) not null,
+     priModel varchar(100) not null,
      priTechnology char(1) not null,
-     priSpeed char(1) not null,
-     priCapacity char(1) not null,
-     priWeight char(1) not null,
-     priResolution char(1) not null,
-     priHeight char(1) not null,
-     priWidth char(1) not null,
-     priDepth char(1) not null,
-     priPrice char(1) not null,
+     priSpeed int not null,
+     priCapacity int not null,
+     priWeight float(1) not null,
+     priResolution int not null,
+     priHeight int not null,
+     priWidth int not null,
+     priDepth int not null,
+     priPrice int not null,
      constraint ID_t_printer_ID primary key (idPrinter));
 
 
@@ -60,14 +61,14 @@ alter table buy add constraint FKbuy_t_c
 -- Index Section
 -- _____________ 
 
-create unique index ID_t_client_IND
-     on t_client (idClient);
-
 create unique index ID_buy_IND
      on buy (idClient, idPrinter);
 
 create index FKbuy_t_p_IND
      on buy (idPrinter);
+
+create unique index ID_t_client_IND
+     on t_client (idClient);
 
 create unique index ID_t_printer_IND
      on t_printer (idPrinter);
