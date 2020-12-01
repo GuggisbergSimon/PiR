abc7faff42c8aab5edd275b26aea836ac488d8ae
Elias <elias.vuille@eduvaud.ch>
2020-12-01 15:09:58 +0100

    Apercu de l'index

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>printlookup - Votre comparateur d'imprimante</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<header>
+   <div class="jumbotron jumbotron-fluid">
+       <h1 class="display-1">printlookup - Comparateur d'imprimantes</h1>
+   </div>
+</header>
+<?php
+
+?>
+</body>
+
+
+</html>
