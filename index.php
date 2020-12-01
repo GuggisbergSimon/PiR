91ae432a633f27b104ac37bb7119e0a3a869fadd
Elias <elias.vuille@eduvaud.ch>
2020-12-01 15:47:41 +0100

    AJout classe database et aperçu global du site

--- index.php
+++ index.php
@@ -12,6 +12,43 @@
        <h1 class="display-1">printlookup - Comparateur d'imprimantes</h1>
    </div>
 </header>
+<main>
+    <h1>Les meilleures ventes : (5 meilleures ventes sur 3 ans)</h1><br>
+    <h1>10 meillleures imprimantes selon leurs vitesse d'impression :</h1><br>
+    <h1>Les 5 imprimantes à plus haute résolution</h1><br>
+    <h1>Les trois imprimantes les plus chères du marché</h1><br>
+    <h1>Les trois imprimantes les moins chères du marché</h1><br><br>
+    <h1>Tableau des imprimantes :</h1><br>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">Marque</th>
+            <th scope="col">Modèle</th>
+            <th scope="col">Technologie</th>
+            <th scope="col">Vitesse</th>
+            <th scope="col">Capacité papier</th>
+            <th scope="col">Poids</th>
+            <th scope="col">Résolution</th>
+            <th scope="col">Dimensions</th>
+            <th scope="col">Prix</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Epson</td>
+                <td>CD420</td>
+                <td>Laser</td>
+                <td>500LPM</td>
+                <td>1000 feuilles A4</td>
+                <td>2kg</td>
+                <td>455PPI</td>
+                <td>300*400*200</td>
+                <td>399.-</td>
+            </tr>
+        </tbody>
+    </table>
+</main>
+</body>
 <?php
 
 ?>
